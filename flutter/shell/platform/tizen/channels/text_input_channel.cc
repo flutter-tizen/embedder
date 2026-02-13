dd2f6ba563596fa04d08c0b39c4a63a951c5a8f4
JunsuChoi <junduru019@gmail.com>
2026-02-13 15:11:16 +0900

    Fix composingExtent parsing in setEditingState (#156)

--- flutter/shell/platform/tizen/channels/text_input_channel.cc
+++ flutter/shell/platform/tizen/channels/text_input_channel.cc
@@ -162,6 +162,7 @@ void TextInputChannel::HandleMethodCall(
     if (client_config.IsNull()) {
       result->Error(kBadArgumentError,
                     "Could not set client, missing arguments.");
+      return;
     }
 
     client_id_ = client_id_json.GetInt();
@@ -257,7 +258,7 @@ void TextInputChannel::HandleMethodCall(
         TextRange(selection_base_value, selection_extent_value));
 
     auto composing_base = args.FindMember(kComposingBaseKey);
-    auto composing_extent = args.FindMember(kComposingBaseKey);
+    auto composing_extent = args.FindMember(kComposingExtentKey);
     int composing_base_value = composing_base != args.MemberEnd()
                                    ? composing_base->value.GetInt()
                                    : -1;
