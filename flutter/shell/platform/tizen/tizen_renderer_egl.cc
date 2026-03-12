4273a176cc7be1266fe0de9418c796b1100cec58
JunsuChoi <jsuya.choi@samsung.com>
2026-03-12 19:13:38 +0900

    Support Tizen 11

--- flutter/shell/platform/tizen/tizen_renderer_egl.cc
+++ flutter/shell/platform/tizen/tizen_renderer_egl.cc
@@ -9,7 +9,11 @@
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 #ifdef NUI_SUPPORT
+#if TIZEN_API_VERSION_NUM >= 110
+#include <dali/devel-api/adaptor-framework/native-image-queue.h>
+#else
 #include <dali/devel-api/adaptor-framework/native-image-source-queue.h>
+#endif
 #endif
 #include <tbm_dummy_display.h>
 #include <tbm_surface.h>
@@ -101,6 +105,13 @@ bool TizenRendererEgl::CreateSurface(void* render_target,
           reinterpret_cast<EGLNativeWindowType>(egl_window), attribs);
     } else {
 #ifdef NUI_SUPPORT
+#if TIZEN_API_VERSION_NUM >= 110
+      Dali::NativeImageQueuePtr dali_native_image_queue =
+          static_cast<Dali::NativeImageQueue*>(render_target);
+      tbm_surface_queue_h tbm_surface_queue_ =
+          Dali::AnyCast<tbm_surface_queue_h>(
+              dali_native_image_queue->GetNativeImageQueue());
+#else
       Dali::NativeImageSourceQueuePtr dali_native_image_queue =
           static_cast<Dali::NativeImageSourceQueue*>(render_target);
       tbm_surface_queue_h tbm_surface_queue_ =
@@ -109,6 +120,7 @@ bool TizenRendererEgl::CreateSurface(void* render_target,
       egl_surface_ = eglCreateWindowSurface(
           egl_display_, egl_config_,
           reinterpret_cast<EGLNativeWindowType>(tbm_surface_queue_), attribs);
+#endif
 #endif
     }
 
