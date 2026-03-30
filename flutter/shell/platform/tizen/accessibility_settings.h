4c9b74606d6aec91c4775a7355078da2061ab6d3
Seungsoo Lee <seungsoo47.lee@samsung.com>
2026-03-30 21:07:17 +0900

    Add gdbus callback for Aurum

--- flutter/shell/platform/tizen/accessibility_settings.h
+++ flutter/shell/platform/tizen/accessibility_settings.h
@@ -5,6 +5,7 @@
 #ifndef EMBEDDER_ACCESSIBILITY_SETTINGS_H_
 #define EMBEDDER_ACCESSIBILITY_SETTINGS_H_
 
+#include <gio/gio.h>
 #include <system/system_settings.h>
 
 namespace flutter {
@@ -17,13 +18,27 @@ class AccessibilitySettings {
   virtual ~AccessibilitySettings();
 
  private:
+  void UpdateSemanticsState();
+  void InitializeA11yMonitoring();
+  void InitializeScreenReaderMonitoring();
+  void InitializeHighContrastMonitoring();
+
   static void OnHighContrastStateChanged(system_settings_key_e key,
                                          void* user_data);
   static void OnScreenReaderStateChanged(system_settings_key_e key,
                                          void* user_data);
+  static void OnA11yPropertiesChanged(
+      GDBusProxy* proxy,
+      GVariant* changed_properties,
+      const gchar* const* invalidated_properties,
+      gpointer user_data);
 
   [[maybe_unused]] FlutterTizenEngine* engine_;
   [[maybe_unused]] bool screen_reader_enabled_ = false;
+  bool tts_enabled_ = false;
+  bool a11y_enabled_ = false;
+  GDBusProxy* a11y_proxy_ = nullptr;
+  GCancellable* cancellable_ = nullptr;
 };
 
 }  // namespace flutter
